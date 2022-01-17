a4644b6ea56890ab2eb9d24e21c1e5757710ca51
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-01-17 21:13:27 +1100

    Set up monorepo after fashion of prior monorepo and rust tutorial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prerequisites
+
+Prior to install you must have `make`, `lerna` and `yarn` installed globally.
+
+# Setup
+
+From the root of the project, run `make install`.
--- scripts/run_e2e.sh
+++ scripts/run_e2e.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-case "${PACKAGE}" in
-	retirement)
-		BASEURL="https://portal-robo-primary.aws-technet-dev-au.iress.online"
-		CYPRESS_SPEC="cypress/integration/e2e/gb/*"
-	;;
-	goals)
-		BASEURL="https://goals-primary.aws-technet-dev-au.iress.online"
-		CYPRESS_SPEC="cypress/integration/e2e/*"
-	;;
-	risk-profiler)
-		BASEURL="https://risk-profiler-primary.aws-technet-dev-au.iress.online"
-		CYPRESS_SPEC="cypress/integration/e2e/*"
-	;;
-esac
-
 username=$(sed -n "1 p" "test_account.txt")
 password=$(sed -n "2 p" "test_account.txt")
 
 cd "${E2E_ROOT}"
 cat <<-EOF > cypress.env.json
 {
-	"baseUrl": "${BASEURL}",
+	"baseUrl": "BASE_URL",
 	"apiEnv": "development",
 	"LOCALE": "GB",
 	"username": "${username}",
