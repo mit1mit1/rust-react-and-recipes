a61c9727400ce84a4d2571faf271a1d4ffa43197
mitchellroberts11 <mitchell.roberts@iress.com>
2022-01-20 09:18:24 +1100

    feat: add lint hooks

---
+++ packages/rust-recipes/e2e/cypress/integration/front_page_spec.ts
@@ -0,0 +1,6 @@
+describe("Test the front page", () => {
+  it("Checks the title renders", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Recipe");
+  });
+});
--- packages/rust-recipes/ui/src/App.tsx
+++ packages/rust-recipes/ui/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 enum RecipeStatus {
   DRAFT,
@@ -8,10 +8,10 @@ enum RecipeStatus {
 }
 
 interface Recipe {
-  author: string,
-  title: string,
-  status: RecipeStatus,
-  steps: Array<string>
+  author: string;
+  title: string;
+  status: RecipeStatus;
+  steps: Array<string>;
 }
 
 const recipe_list: Array<Recipe> = [
@@ -19,35 +19,45 @@ const recipe_list: Array<Recipe> = [
     author: "Mitch",
     title: "Nachos",
     status: RecipeStatus.TESTED,
-    steps: ["Get chips", "Get cheese", "Cook up"]
+    steps: ["Get chips", "Get cheese", "Cook up"],
   },
   {
     author: "Anon",
     title: "Salmon Pasta",
     status: RecipeStatus.DRAFT,
-    steps: ["Fry salmon with skin on", "Boil pasta", "Toss together with canned pesto"]
+    steps: [
+      "Fry salmon with skin on",
+      "Boil pasta",
+      "Toss together with canned pesto",
+    ],
   },
-]
+];
 
 interface RecipeProps {
-  recipe: Recipe
+  recipe: Recipe;
 }
 
 const Recipe = (props: RecipeProps) => {
-  return <>
-    <h2>{props.recipe.title}</h2>
-    <h3>By {props.recipe.author}</h3>
-    {props.recipe.steps.map(function(step, i){
-        return <div>{i}. {step}</div>;
-    })}
-  </>
-}
+  return (
+    <>
+      <h2>{props.recipe.title}</h2>
+      <h3>By {props.recipe.author}</h3>
+      {props.recipe.steps.map(function (step, i) {
+        return (
+          <div>
+            {i}. {step}
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <div>Recipes!</div>
-      {recipe_list.map(function(recipe, i){
+      {recipe_list.map(function (recipe, i) {
         return <Recipe recipe={recipe} />;
       })}
     </>
